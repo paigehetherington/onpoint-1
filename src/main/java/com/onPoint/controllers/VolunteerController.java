70e079b7273a66096208a4a537ddb1baabfe71c5
Paige Hetherington <herbmama@gmail.com>
2016-04-05 15:59:58 -0400

    initial import

---
+++ src/main/java/com/onPoint/controllers/VolunteerController.java
@@ -0,0 +1,8 @@
+package com.onPoint.controllers;
+
+/**
+ * Created by vajrayogini on 4/5/16.
+ */
+@RestController
+public class VolunteerController {
+}
---
+++ src/main/java/com/onPoint/services/VolunteerRepository.java
@@ -0,0 +1,7 @@
+package com.onPoint.services;
+
+/**
+ * Created by vajrayogini on 4/5/16.
+ */
+public class VolunteerRepository {
+}
