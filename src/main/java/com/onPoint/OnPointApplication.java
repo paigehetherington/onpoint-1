71e96ff0ea59d32b4f04a6b8b474184d3b5d2069
Candace Cohen <cecoh611@gmail.com>
2016-04-06 11:51:15 -0400

    added readme file

---
+++ src/main/java/com/onPoint/OnPointApplication.java
@@ -0,0 +1,12 @@
+package com.onPoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnPointApplication.class, args);
+	}
+}
