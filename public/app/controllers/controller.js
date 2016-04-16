1440ed014a2fd07ee29f3cf875099dce9c71d134
Candace Cohen <cecoh611@gmail.com>
2016-04-15 22:44:34 -0400

    added photos

--- public/app/controllers/controller.js
+++ public/app/controllers/controller.js
@@ -1,21 +1,38 @@
 angular
   .module('onpoint')
-  .controller("MainCtrl", function($scope, $location,
+  .controller("MainCtrl", function($scope, $location, $window,
     onPointService) {
 
       $scope.volunteers = [];
-
+      $scope.testing = 'hello';
+      var getToken = JSON.parse($window.sessionStorage.getItem('token'));
+      if(getToken) {
+        console.log(getToken)
+        $scope.userAuthenticated = true;
+        $scope.userData = getToken.data;
+      } else {
+        //do some logic
+      }
       $scope.loginUser = function(user) {
           onPointService.login(user)
-          .success(function(data) {
-            console.log("USER LOGGED IN ", data);
-            // $scope.
+          .then(function(data) {
+            var stringifyResponse = JSON.stringify(data);
+            $window.sessionStorage.setItem('token', stringifyResponse);
+            $scope.userAuthenticated = true;
+            var getToken = JSON.parse($window.sessionStorage.getItem('token'));
+            $scope.userData = getToken.data;
+            $location.path('/');
+          }).catch(function(e) {
+            console.error('An error occured logging in:', e);
           });
       };
 
-      $scope.login = function(){
+      $scope.logout = function() {
+        $window.sessionStorage.removeItem('token');
+        $scope.userAuthenticated = false;
+        $scope.userData = null;
         $location.path('/');
-      };
+      }
 
       $scope.scrollTo = function(image,ind) {
         $scope.listposition = {left:(IMAGE_WIDTH * ind * -1) + "px"};
